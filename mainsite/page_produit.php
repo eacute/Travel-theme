99054b8a09f999d41da75e7201e1f4b05a0f3505
eacute <caipirinha@pt.lu>
2012-10-22 13:42:52 +0200

    Fix colors
    Fix sprites

--- mainsite/page_produit.php
+++ mainsite/page_produit.php
@@ -20,7 +20,8 @@
 		<link type="text/css" href="../webroot/css/ie8.css" rel="stylesheet">
 	<![endif]-->
 	<link type="text/css" href="../webroot/css/print.css" rel="stylesheet" media="print">
-	
+		<link type="text/css" href="../webroot/css/colors/rouge.css" rel="stylesheet">
+
 	<script type="text/javascript" src="../webroot/js/theme_front-socle/jquery-1.7.2.min.js"></script>
 	<script type="text/javascript" src="../webroot/js/theme_front-socle/jquery-ui-effect.min.js"></script>
 	<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=xa-4f6e9cb7474031f4"></script>
@@ -56,7 +57,7 @@
 			<div class="container">
 				<ul class="nav">
 					<li class="active"><a title="" href="#labels">Labels</a></li>
-					<li class="dropdown">
+					<li class="active dropdown">
 						<a title="" class="dropdown-toggle" data-toggle="dropdown" href="#">Navigation</a>
 						<ul class="dropdown-menu">
 							<li class=""><a title="" href="#navs">Nav, tabs, pills</a></li>
